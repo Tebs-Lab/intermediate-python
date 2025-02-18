076e771f9e7aad91e282f7f1df445cc1bc36cdc4
Tyler Bettilyon <teb@tebs-lab.com>
2025-02-18 11:41:55 -0700

    extended the multiple inheritance short example

--- 03_classes_2/0x_bonus_multiple_inheritance.py
+++ 03_classes_2/0x_bonus_multiple_inheritance.py
@@ -16,4 +16,13 @@ class C(A, B):
         super().__init__()
         self.c = 'C'
 
+# Run this code once, then again after making the following changes:
+# 1. Change the order of A and B in the class definition for C.
+# 2. Comment out the call to super().__init__() in class A.
+# 3. Comment out the call to super().__init__() in class B.
+
 c = C()
+
+# For more details:
+# https://realpython.com/python-super/
+# 
